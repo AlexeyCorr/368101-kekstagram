e502bc3a4b4fbae7bb28c462a4afe65233c02adc
AlexeyCorr <alexeycorr@gmail.com>
2018-02-18 16:22:57 +0300

    Блок 4. Задание 13.

--- js/pictures.js
+++ js/pictures.js
@@ -12,7 +12,18 @@ var PICTURE_OPTIONS = {
   },
   COMMENTS: ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!']
 };
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 var gallery = document.querySelector('.gallery-overlay');
+var galleryClose = gallery.querySelector('.gallery-overlay-close');
+var uploadForm = document.querySelector('.upload-form');
+var uploadField = uploadForm.querySelector('#upload-file');
+var filterForm = uploadForm.querySelector('.upload-overlay');
+var buttonClose = filterForm.querySelector('.upload-form-cancel');
+var effectControlsField = filterForm.querySelector('.upload-effect-controls');
+var effectControlPin = effectControlsField.querySelector('.upload-effect-level-pin');
+var pictureReview = filterForm.querySelector('.effect-image-preview');
+var picturesField = document.querySelector('.pictures');
 
 // Получение случайного значения
 var getRandomValue = function (min, max) {
@@ -22,6 +33,25 @@ var getRandomValue = function (min, max) {
 // Удаление класса у элемента
 var removeClass = function (element, className) {
   element.classList.remove(className);
+};
+
+// Добавляет класс элементу
+var addClass = function (element, className) {
+  element.classList.add(className);
+};
+
+// Выполнить какое-то действие при нажатии на ESC
+var isEscEvent = function (evt, action) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    action();
+  }
+};
+
+// Выполнить какое-то действие при нажатии на ENTER
+var isEnterEvent = function (evt, action) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    action();
+  }
 };
 
 // Получение массива с индексами изображений
@@ -80,7 +110,7 @@ var createElementPicture = function (array) {
   var elementPhoto = similarPhotoTemplate.cloneNode(true);
 
   elementPhoto.querySelector('img').src = array.url;
-  elementPhoto.querySelector('.picture-comments').textContent = array.comments;
+  elementPhoto.querySelector('.picture-comments').textContent = array.comments.length;
   elementPhoto.querySelector('.picture-likes').textContent = array.likes;
 
   return elementPhoto;
@@ -97,6 +127,91 @@ var drawElementPhoto = function () {
   return similarPhotoList.appendChild(fragment);
 };
 
-removeClass(gallery, 'hidden');
 getArrayPicture(PICTURE_OPTIONS.AMOUNT);
 drawElementPhoto();
+
+// ---ОБРАБОТКА СОБЫТИЙ---
+
+// Ф-я для открытия попапа редактирования фотографии
+var onInputChange = function (evt) {
+  evt.preventDefault();
+  removeClass(filterForm, 'hidden');
+  document.addEventListener('keydown', onUploadEscKeydown);
+  buttonClose.addEventListener('keydown', function () {
+    isEnterEvent(evt, uploadFormClose);
+  });
+};
+
+// Ф-я закрытия попапа редактирования
+var uploadFormClose = function () {
+  addClass(filterForm, 'hidden');
+  uploadForm.reset();
+  document.removeEventListener('keydown', onUploadEscKeydown);
+};
+
+// Ф-я закрытия попапа редактирования по нажатию на ESC
+var onUploadEscKeydown = function (evt) {
+  isEscEvent(evt, uploadFormClose);
+};
+
+// События на попапе редактирования фото
+
+// Открытие по change
+uploadField.addEventListener('change', onInputChange);
+
+// Закрытие по клику
+buttonClose.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  uploadFormClose();
+});
+
+// Применение различных эффектов
+effectControlsField.addEventListener('change', function (evt) {
+  var targetInput = evt.target.closest('input[type=radio]');
+  pictureReview.setAttribute('class', 'effect-image-preview');
+
+  if (targetInput) {
+    pictureReview.classList.add('effect-' + targetInput.value + '');
+  }
+});
+
+// События при изменении полоски интенсивности
+effectControlPin.addEventListener('mouseup', function () {
+
+});
+
+// Ф-я закрытия попапа показа фото по нажатию на ESC
+var onReviewEscKeydown = function (evt) {
+  isEscEvent(evt, closePopupReview);
+};
+
+// Ф-я открытия попапа показа фото
+var openPopupReview = function (evt) {
+  var targetPicture = evt.target.closest('.picture');
+  if (targetPicture) {
+    document.addEventListener('keydown', onReviewEscKeydown);
+    removeClass(gallery, 'hidden');
+    gallery.querySelector('img').src = targetPicture.querySelector('img').src;
+    gallery.querySelector('.likes-count').textContent = targetPicture.querySelector('.picture-likes');
+    gallery.querySelector('.likes-count').textContent = targetPicture.querySelector('.picture-likes').textContent;
+    gallery.querySelector('.comments-count').textContent = targetPicture.querySelector('.picture-comments').textContent;
+  }
+};
+
+// Ф-я закрытия попапа показа фото
+var closePopupReview = function () {
+  addClass(gallery, 'hidden');
+  document.removeEventListener('keydown', onReviewEscKeydown);
+};
+
+// Событие для открытия попапа показа фото
+picturesField.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  openPopupReview(evt);
+});
+
+// Событие для закрытия попапа показа фото
+galleryClose.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  closePopupReview();
+});
